4cfa6dbdcd153f9697ce9fb974b2cf9da6c476b2
KotiMango <rons20000@gmail.com>
2021-05-23 14:06:42 +0300

    Creation of GameDetail Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
+//Redux
 import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+//Components
+import GameDetail from "../components/GameDetail";
 import Game from "../components/Game";
+//Styling and animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
@@ -15,6 +19,7 @@ const Home = () => {
   const { popular, newGames, upComing } = useSelector((state) => state.games);
   return (
     <GameList>
+      <GameDetail />
       <h2>Upcoming Games</h2>
       <Games>
         {upComing.map((game) => (
